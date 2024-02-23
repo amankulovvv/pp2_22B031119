f6c2412d2e434b175858ce2a575bcbcdb4c87ad0
amankulovvv <mikoamankulov323@gmail.com>
2024-02-23 09:31:02 +0600

    add new commit

---
+++ TSIS4/math/ex2.py
@@ -0,0 +1,5 @@
+height = int(input('Height: '))
+baseFirst = int(input("Base, first value: "))
+baseSecond = int(input("Base, second value: "))
+output = (height*(baseFirst + baseSecond))/2
+print("Expected Output:", output)
---
+++ TSIS4/math/ex4.py
@@ -0,0 +1,4 @@
+baseL = int(input("Length of base: "))
+height = int(input("Height of parallelogram: "))
+output = baseL * height
+print("Extected Output:", output)
