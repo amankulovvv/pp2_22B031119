126593a2f9d415aab67355daada1a7e4d7686963
amankulovvv <mikoamankulov323@gmail.com>
2024-02-24 20:45:24 +0600

    add new commit

---
+++ TSIS5/ex2.py
@@ -0,0 +1,9 @@
+import re
+condition = r'a[b]{2,3}'
+
+string = input()
+
+if re.match(condition, string):
+    print(f"{string} follows condition.")
+else:
+    print(f"{string} does not follow condition.")
